ce2ff36c798340b9fb792f08e19e488d4168144f
Husna Rizkyah <husnarizkyah@gmail.com>
2025-11-21 20:43:43 +0700

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+
+    int usia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void main(){
+        int umur;//variabel
+    }
+}
+class motor{
+    
+}
